715b998cb37940d265fa22454b2948b0a2b527d1
Attila Soos <ats317@g.harvard.edu>
2019-07-08 23:09:34 -0700

    Changes

--- routes.py
+++ routes.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
+    model = joblib.load('lab2_soos.pkl')
     prediction = model.predict([[4, 2.5, 3005, 15, 17903.0]]).round(1)
     prediction.tostring()
     return render_template("index.html", prediction=prediction)
 
 
 if __name__ == '__main__':
-    model = joblib.load('lab2_soos.pkl')
+
     app.run(debug=True)
