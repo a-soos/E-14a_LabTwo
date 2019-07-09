f5d3444ad87367e8ee914cc8e06aea7010161005
Attila Soos <ats317@g.harvard.edu>
2019-07-08 22:58:42 -0700

    Lab 2 Complete

---
+++ routes.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from sklearn.externals import joblib
+from sklearn.tree import DecisionTreeRegressor
+
+regr = DecisionTreeRegressor(random_state=0)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    prediction = model.predict([[4, 2.5, 3005, 15, 17903.0]]).round(1)
+    prediction.tostring()
+    return render_template("index.html", prediction=prediction)
+
+
+if __name__ == '__main__':
+    model = joblib.load('lab2_soos.pkl')
+    app.run(debug=True)
